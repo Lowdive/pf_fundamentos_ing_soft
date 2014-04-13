0be3a8de480f99bd22f94e6a9ad729d87d437ff0
Lowdive <elisantiago.4e@gmail.com>
2014-04-13 01:08:57 -0500

    Pais avance con los métodos básicos
    el puro prototipo

--- PF_Fundamentos_Ing_Software/Pais.h
+++ PF_Fundamentos_Ing_Software/Pais.h
@@ -3,4 +3,11 @@ class Pais
 private:
     int id;
     string nombre;
+    Pais(); // inicializar con nada
+    Pais(string ident); // "<id>",""
+    Pais(string ident, string nom); // "<id>", "nom"
+    string getIdent();
+    void setIdent(string i);
+    string getDat();
+    void setDat(string n);
 };
