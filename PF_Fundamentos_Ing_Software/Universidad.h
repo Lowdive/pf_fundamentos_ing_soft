23cee738fc931d89171cb2089206bdbc4dbb5e8e
Lowdive <elisantiago.4e@gmail.com>
2014-04-14 09:49:02 -0500

    Clase universidad completa
    con todos los métodos funcionando con pruebas en main

--- PF_Fundamentos_Ing_Software/Universidad.h
+++ PF_Fundamentos_Ing_Software/Universidad.h
@@ -3,4 +3,56 @@ class Universidad
 private:
     int id;
     string nombre;
+public:
+    Universidad(); // inicializar con nada
+    Universidad(int ident); // "<id>",""
+    Universidad(int ident, string nom); // "<id>", "nom"
+    int getUIdent();
+    void setUIdent(int i);
+    string getUNom();
+    void setUNom(string n);
+    void muestraUni();
 };
+
+Universidad::Universidad()
+{
+    id=1;
+    nombre="ITESM";
+}
+
+Universidad::Universidad(int ident)
+{
+    id=ident;
+    nombre="ITESM";
+}
+
+Universidad::Universidad(int ident, string nom)
+{
+    id=ident;
+    nombre=nom;
+}
+
+int Universidad::getUIdent()
+{
+    return id;
+}
+
+void Universidad::setUIdent(int i)
+{
+    id=i;
+}
+
+string Universidad::getUNom()
+{
+    return nombre;
+}
+
+void Universidad::setUNom(string n)
+{
+    nombre=n;
+}
+
+void Universidad::muestraUni()
+{
+    cout << id << " " << nombre << endl;
+}
--- PF_Fundamentos_Ing_Software/main.cpp
+++ PF_Fundamentos_Ing_Software/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 using namespace std;
 #include "Pais.h"
+#include "Universidad.h"
 
 int main()
 {
@@ -13,6 +14,16 @@ int main()
     conIDNom.setIdent(3);
     conIDNom.setNom("Brasil");
     conIDNom.muestraPais();
+
+    Universidad udef;
+    Universidad uconID;
+    Universidad uconIDNom;
+    udef.muestraUni();
+    uconID.setUIdent(5);
+    uconID.muestraUni();
+    uconIDNom.setUIdent(3);
+    uconIDNom.setUNom("Brasil");
+    uconIDNom.muestraUni();
     cout << "Hello world!" << endl;
     return 0;
 }
