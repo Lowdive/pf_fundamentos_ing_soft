24648a8eacb6128e82c4f65c61cf42450f282f18
Lowdive <elisantiago.4e@gmail.com>
2014-04-17 23:49:26 -0500

    Clase programa avance
    NO está completa, no incluir en main aún.

--- PF_Fundamentos_Ing_Software/Programa.h
+++ PF_Fundamentos_Ing_Software/Programa.h
@@ -5,7 +5,44 @@ private:
     string carreras[500];
     int capacidad;
     string tipo;
+public:
+    Programa(); // inicializar con nada
+    Programa(string nom); // "<id>",""
+    Programa(string nom, string arr[], int cap, string tip); // "<id>", "nom"
+    string getProg();
+    void setProg(string program);
+    string getCarr();
+    void setNewCarrera(string carr); //adds to the end of the arr
+    void delCarrera(string carr); //searches for the career then deletes then fixes empty spaces
+    int getCap();
+    void setCap(int c);
+    string getTipo();
+    void setTipo(string t);
 };
+
+Programa::Programa()
+{
+    nombre="Intercambio";
+    carreras={"INT", "ITC", "ITIC"};
+    capacidad=60;
+    tipo="normal";
+}
+
+Programa::Programa(string nom)
+{
+    nombre=nom;
+    carreras={"INT", "ITC", "ITIC"};
+    capacidad=60;
+    tipo="normal";
+}
+
+Programa(string nom, string arr[], int cap, string tip)
+{
+    nombre=nom;
+    carreras=arr;
+    capacidad=60;
+    tipo="normal";
+}
 
 void Programa::editarPrograma()
 {
