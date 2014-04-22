81e9776cd0a316a03487e3f22ab7d2e1ef48f26c
Lowdive <elisantiago.4e@gmail.com>
2014-04-21 23:08:21 -0500

    picoteando aquí y allá en usuario
    avances en clase usuario

--- PF_Fundamentos_Ing_Software/Usuario.h
+++ PF_Fundamentos_Ing_Software/Usuario.h
@@ -4,6 +4,14 @@ private:
     string nombre;
     int id;
     string pass;
+public:
+    string getUser();
+    void setUser();
+    int getIDu();
+    void setIDu();
+    string getPass();
+    void setPass();
+    bool autenticar(string pass);
 };
 
 bool Usuario::autenticar(string pass)
