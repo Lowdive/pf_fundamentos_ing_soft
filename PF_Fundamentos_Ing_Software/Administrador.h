392493f8cf5af4b147f1a27af9aaf071fafab41d
Lowdive <elisantiago.4e@gmail.com>
2014-04-30 23:40:26 -0500

    Avanzando clase administrador
    ok

--- PF_Fundamentos_Ing_Software/Administrador.h
+++ PF_Fundamentos_Ing_Software/Administrador.h
@@ -3,4 +3,12 @@ class Administrador
 private:
     int id;
     string nombre;
+public:
+    Administrador();
+    Administrador(int ID, string nom);
+    int getIDadmin();
+    void setIDadmin(int newID);
+    string getNomAdmin();
+    void setNomAdmin(string newAdmin);
+
 };
